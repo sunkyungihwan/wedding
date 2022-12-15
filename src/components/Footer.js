ee09cc927ebee6c9332b23cb0797086fe9593aa2
sunky0 <windo3o@hanmail.net>
2022-12-15 09:13:37 +0900

    preview

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import { WEDDING_INVITATION_URL } from '../utils/config';
 
 const Information = () => {
+    const doCopy = (text) => {
+        // 흐음 1.
+        if (navigator.clipboard) {
+            // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
+            navigator.clipboard
+                .writeText(text)
+                .then(() => {
+                    alert('복사를 완료했습니다.');
+                })
+                .catch(() => {
+                    alert('복사를 다시 시도해주세요.');
+                });
+        } else {
+            // 흐름 2.
+            if (!document.queryCommandSupported('copy')) {
+                return alert('복사하기가 지원되지 않는 브라우저입니다.');
+            }
+
+            // 흐름 3.
+            const textarea = document.createElement('textarea');
+            textarea.value = text;
+            textarea.style.top = 0;
+            textarea.style.left = 0;
+            textarea.style.position = 'fixed';
+
+            // 흐름 4.
+            document.body.appendChild(textarea);
+            // focus() -> 사파리 브라우저 서포팅
+            textarea.focus();
+            // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+            textarea.select();
+            // 흐름 5.
+            document.execCommand('copy');
+            // 흐름 6.
+            document.body.removeChild(textarea);
+
+            alert('복사를 완료했습니다.');
+        }
+    };
     return (
         <div className="content footer">
             <div className="shared">
-                <button>카카오톡으로 공유하기</button>
-                <button>청첩장 주소 복사하기</button>
+                <button onClick={() => doCopy(WEDDING_INVITATION_URL)}>청첩장 주소 복사하기</button>
             </div>
             <div className="copyright">
                 Copyright 2022. <span>Sunkyungihwan</span>. All rights reserved.
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
-export const WEDDING_INVITATION_URL = 'http://localhost:8080';
+export const WEDDING_INVITATION_URL = 'https://sunkyungihwan.github.io/wedding';
 export const KAKAOTALK_API_TOKEN = '카카오톡 API 토큰을 입력해주세요!';
 export const KAKAOTALK_SHARE_IMAGE = '카카오톡으로 공유할 때 사용되는 대표이미지 URL을 입력해주세요!';
 
@@ -15,7 +15,7 @@ export const ACCOUNT_INFO = [
                 name: '이기환',
                 account_bank: '하나은행',
                 account_number: '748-910761-58507',
-                kakaopay: 'https://qr.kakaopay.com/Ej837Wv6o'
+                kakaopay: 'https://qr.kakaopay.com/Fa5YOcpWI'
             }
         ]
     },
