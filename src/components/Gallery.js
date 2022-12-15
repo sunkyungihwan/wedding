23a6c4712928c917b6caaf804d2051c5005e73fd
sunky0 <sunkyung.choi@lge.com>
2022-12-15 23:17:34 +0900

    image2

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,10 +4,20 @@ import useScrollFadeIn from '..//hooks/useScrollFadeIn';
 import photo1 from '../assets/img/pic01.jpg';
 import photo2 from '../assets/img/pic02.jpg';
 import photo3 from '../assets/img/pic03.jpg';
+import photo4 from '../assets/img/pic04.jpg';
+import photo5 from '../assets/img/pic05.jpg';
+import photo6 from '../assets/img/pic06.jpg';
+import photo7 from '../assets/img/pic07.jpg';
+import photo8 from '../assets/img/pic08.jpg';
 const images = [
     { original: photo1, thumbnail: photo1 },
     { original: photo2, thumbnail: photo2 },
-    { original: photo3, thumbnail: photo3 }
+    { original: photo3, thumbnail: photo3 },
+    { original: photo4, thumbnail: photo4 },
+    { original: photo5, thumbnail: photo5 },
+    { original: photo6, thumbnail: photo6 },
+    { original: photo7, thumbnail: photo7 },
+    { original: photo8, thumbnail: photo8 }
 ];
 function Gallery() {
     const animatedItem = useScrollFadeIn('up', 1, 0);
--- src/components/Information.js
+++ src/components/Information.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import useScrollFadeIn from '..//hooks/useScrollFadeIn';
-import photo4 from '../assets/img/pic04.JPG';
+import photo from '../assets/img/info.jpg';
 
 const Information = () => {
     const animatedItem = useScrollFadeIn('up', 1, 0);
@@ -8,7 +8,7 @@ const Information = () => {
         <div {...animatedItem} className="content information">
             <h2 className="title">층별 안내</h2>
             <div>
-                <img src={photo4} />
+                <img src={photo} />
             </div>
             <div className="info">
                 <dl className="floor">
