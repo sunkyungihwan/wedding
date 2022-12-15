3f14585bfd0d39b4cbd7f6f592bec1eaf4a85b9f
sunky0 <sunkyung.choi@lge.com>
2022-12-16 00:14:05 +0900

    copy

--- src/components/Account/Accordion.js
+++ src/components/Account/Accordion.js
@@ -1,52 +1,15 @@
 import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import kakaopay from '../../assets/img/icon-kakaopay.png';
 
 function Accordion({ data }) {
     let [isCopied, setCopied] = useState(false);
-    const doCopy = (text) => {
-        // 흐음 1.
-        if (navigator.clipboard) {
-            // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
-            navigator.clipboard
-                .writeText(text)
-                .then(() => {
-                    setCopied(true);
-                    setTimeout(() => {
-                        setCopied(false);
-                    }, 1000);
-                })
-                .catch(() => {
-                    alert('복사를 다시 시도해주세요.');
-                });
-        } else {
-            // 흐름 2.
-            if (!document.queryCommandSupported('copy')) {
-                return alert('복사하기가 지원되지 않는 브라우저입니다.');
-            }
-
-            // 흐름 3.
-            const textarea = document.createElement('textarea');
-            textarea.value = text;
-            textarea.style.top = 0;
-            textarea.style.left = 0;
-            textarea.style.position = 'fixed';
-
-            // 흐름 4.
-            document.body.appendChild(textarea);
-            // focus() -> 사파리 브라우저 서포팅
-            textarea.focus();
-            // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
-            textarea.select();
-            // 흐름 5.
-            document.execCommand('copy');
-            // 흐름 6.
-            document.body.removeChild(textarea);
-            setCopied(true);
-            setTimeout(() => {
-                setCopied(false);
-            }, 1000);
-        }
-    };
+    const copy = () => {
+        setCopied(true);
+        setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+      }
     return (
         <div className="accordion">
             <div className="accordionInfo">
@@ -62,7 +25,9 @@ function Accordion({ data }) {
                         <span>복사완료</span>
                     </div>
                 ) : (
-                    <button onClick={() => doCopy(data.account_number)}>복사하기</button>
+                    <CopyToClipboard text={data.account_number} onCopy={copy}>
+	                    <button>복사하기</button>
+                    </CopyToClipboard>
                 )}
                 {data.kakaopay !== '' ? (
                     <button
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ScrollToTop from './hooks/scrollToTop';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
         <App />
