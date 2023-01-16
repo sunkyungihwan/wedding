87bef3fdc2169e78d63c1164f86d6fa368936858
sunky0 <windo3o@hanmail.net>
2023-01-16 22:52:38 +0900

    picture add

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,6 +9,10 @@ import photo5 from '../assets/img/pic05.jpg';
 import photo6 from '../assets/img/pic06.jpg';
 import photo7 from '../assets/img/pic07.jpg';
 import photo8 from '../assets/img/pic08.jpg';
+import photo9 from '../assets/img/pic09.jpg';
+import phot10 from '../assets/img/pic10.jpg';
+import phot11 from '../assets/img/pic11.jpg';
+import phot12 from '../assets/img/pic12.jpg';
 const images = [
     { original: photo1, thumbnail: photo1 },
     { original: photo2, thumbnail: photo2 },
@@ -17,7 +21,11 @@ const images = [
     { original: photo5, thumbnail: photo5 },
     { original: photo6, thumbnail: photo6 },
     { original: photo7, thumbnail: photo7 },
-    { original: photo8, thumbnail: photo8 }
+    { original: photo8, thumbnail: photo8 },
+    { original: photo9, thumbnail: photo9 },
+    { original: phot10, thumbnail: phot10 },
+    { original: phot11, thumbnail: phot11 },
+    { original: phot12, thumbnail: phot12 }
 ];
 function Gallery() {
     const animatedItem = useScrollFadeIn('up', 1, 0);
